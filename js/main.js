d14704c9d1700aaf0ad3f86a2d6a6e97aa3f17fc
andinet <andinetmn@gmail.comm>
2019-12-16 22:30:09 -0600

    my commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
 })
+
+
+$('a.embed').gdocsViewer();
+
+$('#embedURL').gdocsViewer();
+$('a.embed').gdocsViewer({ width: 400, height: 500 });
